905dc9d12f4942bf7eb360c5787096f4255e8b42
Victor Hugo <victorneivadef8@gmail.com>
2024-04-07 15:10:46 -0300

    Projeto básico criado

---
+++ src/test/java/com/Dev/UserShopping/UserShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dev.UserShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
